f1f45adc52dc0a9fbc129b52669d289232a4596e
Si Chan Park <112904030+fantasticious01@users.noreply.github.com>
2024-11-29 18:32:06 -0500

    Made matcard

--- lyricsApp/src/app/components/song-result/song-result.component.ts
+++ lyricsApp/src/app/components/song-result/song-result.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-song-result',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, RouterLink],
   templateUrl: './song-result.component.html',
   styleUrl: './song-result.component.css'
 })
 export class SongResultComponent {
-
+  // Need to inject the service!!!!!!!!!!!!!!
 }
