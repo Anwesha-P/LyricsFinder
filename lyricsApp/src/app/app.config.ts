1fff40d44c9320f1a0af9799765981f0eb28cee4
Si Chan Park <112904030+fantasticious01@users.noreply.github.com>
2024-12-01 22:14:18 -0500

    Working on AUTH

--- lyricsApp/src/app/app.config.ts
+++ lyricsApp/src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"lyricsfinder-7446e","appId":"1:530212744250:web:a9e63abfa851f179ec9903","storageBucket":"lyricsfinder-7446e.firebasestorage.app","apiKey":"AIzaSyAaYW3_Qs4EXwithLOznkLyu8Fn_TY0P1I","authDomain":"lyricsfinder-7446e.firebaseapp.com","messagingSenderId":"530212744250","measurementId":"G-7X9QGM8D6Y"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
--- lyricsApp/src/app/service/data-service/favorites.service.ts
+++ lyricsApp/src/app/service/data-service/favorites.service.ts
@@ -1,9 +1,50 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { addDoc, collection, collectionData, Firestore, query } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface FirestoreRec {
+  artist: string;
+  lyrics: string;
+  releaseDate: string;
+  songTitle: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritesService {
+  firestore: Firestore = inject(Firestore)
+  public items$: Observable<FirestoreRec[]> | undefined;
 
-  constructor() { }
+  constructor() {
+    this.getData();
+  }
+
+  //NEED TO CHANGE THE COLLECTION NAME!!!
+  getData() {
+    const aCollection = collection(this.firestore, 'users/userID/favorites');
+    const q = query(aCollection);
+
+    //Subscribe to real-time updates
+    this.items$ = collectionData(q, {idField: 'id'}) as Observable<FirestoreRec[]>;
+  }
+
+  //NEED TO CHANGE THE COLLECTION NAME!!!
+  storeSong(data: FirestoreRec) {
+    addDoc(collection(this.firestore, 'users/userID/favorites'),
+    {
+      artist: data.artist,
+      lyrics: data.lyrics,
+      releaseDate: data.releaseDate,
+      songTitle: data.songTitle,
+    }
+  )
+  }
+  
+  //----------------------------------------------------------------------------------------------
+  //NEED TO IMPLEMENT THE DELETESONG METHOD!!!
+  removeSong(data: FirestoreRec) {
+
+  }
+
 }
