e86e08d55271346c04b5d20526f1ba6fd96d99ef
Anwesha Pradhananga <ap45@calvin.edu>
2024-12-01 21:20:01 -0500

    changes name of app

--- lyricsApp/src/app/service/api-service/api.service.ts
+++ lyricsApp/src/app/service/api-service/api.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class APIService {
+  public data$ = signal<Song[]>([])
+  constructor() {this.fetchData()}
+  async fetchData(){
+    try{
+      const response = await fetch('https://fakestoreapi.com/products')
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const json = await response.json();
+      this.data$.set(json)
+      console.log(this.data$())
 
-  constructor() { }
+    } catch (error) {
+      console.error('error');
+    }
+  }
+}
+export interface Song {
+  id:number;
+  title: string;
+  image: string;
+  url: string;
+  artist_names: string;
+  release_date_for_display: string;
+
 }
