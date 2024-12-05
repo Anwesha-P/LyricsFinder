93a4fce9688e1c4ecdecdb3c8e0e3495f1b815d8
Si Chan Park <112904030+fantasticious01@users.noreply.github.com>
2024-12-04 20:57:30 -0500

    implemented

--- lyricsApp/src/app/components/song-result/song-result.component.ts
+++ lyricsApp/src/app/components/song-result/song-result.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { LogoutComponent } from '../logout/logout.component';
+
 
 @Component({
   selector: 'app-song-result',
   standalone: true,
-  imports: [MatCardModule, RouterModule, NavbarComponent, LogoutComponent],
+  imports: [NavbarComponent, LogoutComponent],
   templateUrl: './song-result.component.html',
   styleUrl: './song-result.component.css'
 })
